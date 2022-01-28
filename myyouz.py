853ccd2dea4099a19b573edd946701df39e7f1f8
Agunmyyouz <97822431+Agunmyyouz@users.noreply.github.com>
2022-01-28 14:49:41 +0700

    Add files via upload

--- myyouz.py
+++ myyouz.py
@@ -1,1 +1,19 @@
-encho "WOI BOCIL"
+import requests
+import time,os,sys
+
+# clear 
+os.system('clear')
+# logo
+print ('-------------------------------')
+print ('[+] Creator : Agunmyyouz')
+print ('[+] Youtube : AGUNMYYOUZ')
+print ('-------------------------------')
+print ('\n[+] nomor di awali 8xxxx cuy')
+
+# Run nomor
+nomor = input('[+] Nomor terget : ')
+req=requests.get('https://ainxbot-sms.herokuapp.com/api/spamsms',params={'phone':nomor}).text
+if 'terkirim' in req:
+     print ('[√] Spam Terkirim')
+else:
+     print ('[!] Spam Gagal')
